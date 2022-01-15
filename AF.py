91be550bcefcdf037a4939cdef482de76b09d89b
Alex Adler <project.alex.adler@gmail.com>
2022-01-15 16:21:31 +0100

    Ran code through formatter

--- AF.py
+++ AF.py
@@ -10,7 +10,7 @@ g0 = 9.81  # Standard gravity
 # Mars properties
 rho = 0.02  # Density kg/m3
 a = 240  # Speed of sound m/s
-g = 3.721   # Gravity m/s2
+g = 3.721  # Gravity m/s2
 gamma = 1.31  # Adiabatic gas constant
 surface_pressure = 610
 
@@ -20,11 +20,11 @@ beta_step = 1
 
 
 def calc_CL(L, rho, V, S):
-    return (2*L)/(rho*V**2*S)
+    return (2 * L) / (rho * V ** 2 * S)
 
 
 def calc_V(L, rho, C_L, S):
-    return ((2*L)/(rho*C_L*S))**.5
+    return ((2 * L) / (rho * C_L * S)) ** 0.5
 
 
 def DesignVehicle():
@@ -43,40 +43,45 @@ def DesignVehicle():
 
     # Vehicle properties
     # Weight
-    W_PL_N = payload_kg * g   # Payload weight
+    W_PL_N = payload_kg * g  # Payload weight
     W_C_N = 0  # Crew Weight
-    W_F_N = (F_N/(g0*Isp_s))*flight_time_s*g
-    MTOW_N = W_PL_N + W_C_N+W_F_N
-    MTOW_N *= 1+We_Wto
+    W_F_N = (F_N / (g0 * Isp_s)) * flight_time_s * g
+    MTOW_N = W_PL_N + W_C_N + W_F_N
+    MTOW_N *= 1 + We_Wto
 
-    print(f"MTOW = {MTOW_N:.4} N, Payload Weight = {W_PL_N:.4} N, Fuel Weight = {W_F_N:.4} N ({W_F_N/(1000*g):.4} tonnes), Total Mass = {MTOW_N/(1000*g):.4} tonnes")
+    print(
+        f"MTOW = {MTOW_N:.4} N, Payload Weight = {W_PL_N:.4} N, Fuel Weight = {W_F_N:.4} N ({W_F_N/(1000*g):.4} tonnes), Total Mass = {MTOW_N/(1000*g):.4} tonnes"
+    )
 
     print(f"M = {calc_V(MTOW_N, rho, 0.1, 1000)/a:.4}")
     print(f"C_L = {calc_CL(MTOW_N, rho, 4*a, 1000):.4}")
 
 
 def PG(M):
-    return 1/(1-M**2)
+    return 1 / (1 - M ** 2)
 
 
 def LinearSupersonic(M):
-    return 1/(M**2-1)
+    return 1 / (M ** 2 - 1)
 
 
 def Ackeret(theta_deg, M):
-    return 2 * np.radians(theta_deg)/np.sqrt(M**2-1)
+    return 2 * np.radians(theta_deg) / np.sqrt(M ** 2 - 1)
 
 
 def SupersonicFlatPlate(alpha, M):
     # Sectional lift coeff, Sectional wave drag coeff
-    return (4 * np.radians(alpha)/np.sqrt(M**2-1), 4 * np.radians(alpha)**2/np.sqrt(M**2-1))
+    return (
+        4 * np.radians(alpha) / np.sqrt(M ** 2 - 1),
+        4 * np.radians(alpha) ** 2 / np.sqrt(M ** 2 - 1),
+    )
 
 
 def PlotFuelWeightISP(F_N, flight_time_s):
     # Test Values
     Isp = np.linspace(500, 1000, 100)
 
-    w = (F_N/(g0*Isp))*flight_time_s*g
+    w = (F_N / (g0 * Isp)) * flight_time_s * g
 
     fig, ax = plt.subplots()
     ax.plot(Isp, w)
@@ -91,10 +96,15 @@ def PlotFuelWeightISP(F_N, flight_time_s):
 # Calculates the deflection required for a given Mach number and shock angle beta
 def Oblique(beta, M, gamma):
     beta = np.radians(beta)
-    return np.degrees(np.arctan((2*(M**2*np.sin(beta)**2-1))/(np.tan(beta)*(M**2*(gamma+np.cos(2*beta))+2))))
+    return np.degrees(
+        np.arctan(
+            (2 * (M ** 2 * np.sin(beta) ** 2 - 1))
+            / (np.tan(beta) * (M ** 2 * (gamma + np.cos(2 * beta)) + 2))
+        )
+    )
 
 
-def GetObliqueShockAngle(M, theta_deg, gamma, beta_max=90, beta_step=.1):
+def GetObliqueShockAngle(M, theta_deg, gamma, beta_max=90, beta_step=0.1):
     theta_left = 0
     beta_left = 0
     shock_table = []
@@ -113,30 +123,33 @@ def GetObliqueShockAngle(M, theta_deg, gamma, beta_max=90, beta_step=.1):
             return np.interp(theta_deg, [theta_left, value[1]], [beta_left, value[0]])
 
     print("Normal shock")
-    return(90)
+    return 90
 
 
 # Returns (M2, shock angle, p2/p1, p02/p01, rho2/rho1, T2/T1)
 def CalcObliqueShock(M1, theta_deg, gamma):
     beta_rad = np.radians(GetObliqueShockAngle(M1, theta_deg, gamma))
     theta_rad = np.radians(theta_deg)
-    M1n2 = M1**2*np.sin(beta_rad)**2
-    M2 = np.sqrt(((gamma-1)*M1n2+2) /
-                 ((2*gamma*M1n2-(gamma-1))*np.sin(beta_rad-theta_rad)**2))
-    p2_p1 = (2*gamma*M1n2-(gamma-1))/(gamma+1)
-    rho2_rho1 = ((gamma+1)*M1n2) / \
-        ((gamma-1)*M1n2+2)
-    T2_T1 = ((2*gamma*M1n2-(gamma-1))*((gamma-1)*M1n2+2)) / \
-        ((gamma+1)**2*M1n2)
-    p02_p01 = rho2_rho1**(gamma/(gamma-1))*(p2_p1)**(-1/(gamma-1))
+    M1n2 = M1 ** 2 * np.sin(beta_rad) ** 2
+    M2 = np.sqrt(
+        ((gamma - 1) * M1n2 + 2)
+        / ((2 * gamma * M1n2 - (gamma - 1)) * np.sin(beta_rad - theta_rad) ** 2)
+    )
+    p2_p1 = (2 * gamma * M1n2 - (gamma - 1)) / (gamma + 1)
+    rho2_rho1 = ((gamma + 1) * M1n2) / ((gamma - 1) * M1n2 + 2)
+    T2_T1 = ((2 * gamma * M1n2 - (gamma - 1)) * ((gamma - 1) * M1n2 + 2)) / (
+        (gamma + 1) ** 2 * M1n2
+    )
+    p02_p01 = rho2_rho1 ** (gamma / (gamma - 1)) * (p2_p1) ** (-1 / (gamma - 1))
+    print(beta_rad)
     return M2, np.degrees(beta_rad), p2_p1, p02_p01, rho2_rho1, T2_T1
 
 
 # Get coordinates needed to draw a wing on a plot
 def GetWingCoords(h, alpha):
     alpha = np.radians(alpha)
-    x = [1-np.cos(alpha), 1]
-    y = [h*(1+np.sin(alpha)), h]
+    x = [1 - np.cos(alpha), 1]
+    y = [h + np.sin(alpha), h]
     return (x, y)
 
 
@@ -147,16 +160,16 @@ def GetShockCoords(x1, y1, M, theta_deg, wing_m, wing_c, gamma):
     if y1 == 0:
         # Bounce up
         shock_m = np.tan(np.radians(shock[1]))
-        shock_c = -shock_m*x1
+        shock_c = -shock_m * x1
 
-        x2 = (shock_c-wing_c)/(wing_m-shock_m)
-        y2 = shock_m*x2+shock_c
+        x2 = (shock_c - wing_c) / (wing_m - shock_m)
+        y2 = shock_m * x2 + shock_c
     else:
         # Bounce down
         y2 = 0
-        x2 = x1+(y1-y2)/(np.tan(np.radians(shock[1])))
+        x2 = x1 + (y1 - y2) / (np.tan(np.radians(shock[1])))
 
-    return([x1, x2], [y1, y2], shock)
+    return ([x1, x2], [y1, y2], shock)
 
 
 # Draw the plot showing how the wing interacts
@@ -168,8 +181,8 @@ def DrawSupersonicWing(h, alpha, M, gamma):
 
     # Draw wing
     wing = GetWingCoords(h, alpha)
-    wing_m = (wing[1][0]-wing[1][1])/(wing[0][0]-wing[0][1])
-    wing_c = wing[1][0]-wing[0][0]*wing_m
+    wing_m = (wing[1][0] - wing[1][1]) / (wing[0][0] - wing[0][1])
+    wing_c = wing[1][0] - wing[0][0] * wing_m
     ax.plot(wing[0], wing[1])
 
     x = wing[0][0]
@@ -206,8 +219,8 @@ def DrawSupersonicWing(h, alpha, M, gamma):
                 x = 1
                 y = h
 
-            lift += (x-x_last) * current_pressure
-            drag += (y_last-y) * current_pressure
+            lift += (x - x_last) * current_pressure
+            drag += (y_last - y) * current_pressure
             # print(
             #     f"Adding {((x-x_last) * current_pressure)/(0.5*gamma*M_inf**2)} of lift coeff")
 
@@ -218,25 +231,24 @@ def DrawSupersonicWing(h, alpha, M, gamma):
     # Upper surface
     c_p_u = Ackeret(-alpha, M_inf)
 
-    c_l = lift/(0.5*gamma*M_inf**2) - \
-        c_p_u*np.cos(np.radians(alpha))
-    c_d = drag/(0.5*gamma*M_inf**2) - \
-        c_p_u*np.sin(np.radians(alpha))
+    c_l = lift / (0.5 * gamma * M_inf ** 2) - c_p_u * np.cos(np.radians(alpha))
+    c_d = drag / (0.5 * gamma * M_inf ** 2) - c_p_u * np.sin(np.radians(alpha))
 
     unbounded = SupersonicFlatPlate(alpha, M_inf)
 
     print(f"Lift Coefficient = {c_l:.4f}, Wave Drag Coefficient = {c_d:.4f}")
     print(f"Unbounded flow c_l= {unbounded[0]:.4f}, c_d = {unbounded[1]:.4f}")
     print(
-        f"Gound effect increases c_l by factor of {c_l/unbounded[0]:.2f} and c_d by a factor {c_d/unbounded[1]:.2f}")
+        f"Gound effect increases c_l by factor of {c_l/unbounded[0]:.2f} and c_d by a factor {c_d/unbounded[1]:.2f}"
+    )
 
     # Plot formatting
     plt.title(f"Wing section at alpha = {alpha}, M = {M_inf}, and h = {h} c")
     plt.ylabel(r"$\frac{y}{c}$")
     plt.xlabel(r"$\frac{x}{c}$")
-    plt.xlim([-.5, 1.5])
-    plt.ylim([0, 1.5*h])
-    plt.show()
+    plt.xlim([-0.5, 1.5])
+    plt.ylim([0, 1.5 * h])
+    # plt.show()
 
 
 # Plot altitude vs pressure for Earth and Mars atmospheres
@@ -247,21 +259,21 @@ def Atmosphere():
     # Earth atmosphere
     h = np.linspace(0, 100, 100)
     atmo = coesa76(h)
-    ax.plot(atmo.P/1000, h, c='royalblue')
-    plt.xlim([0, atmo.P[0]/1000])
+    ax.plot(atmo.P / 1000, h, c="royalblue")
+    plt.xlim([0, atmo.P[0] / 1000])
 
     # Mars Atmosphere
     h_mars = np.linspace(0, 100, 100)
-    p_mars = .699*np.exp(-.00009*h_mars*1000)
+    p_mars = 0.699 * np.exp(-0.00009 * h_mars * 1000)
     ax.plot(p_mars, h_mars, c="coral")
     plt.ylim([0, 100])
     # plt.ylabel("Altitude (km)")
     # plt.xlabel("Pressure (kPa)")
-    ax.set_xlabel('Pressure (kPa)')
-    ax.set_ylabel('Altitude (km)')
+    ax.set_xlabel("Pressure (kPa)")
+    ax.set_ylabel("Altitude (km)")
 
     fig.tight_layout()
-    plt.savefig("./big_atmo.svg", transparent=True, bbox_inches='tight')
+    plt.savefig("./big_atmo.svg", transparent=True, bbox_inches="tight")
 
 
 # Plot Mars pressure at Earth equivalent altitude
@@ -272,31 +284,31 @@ def AtmosphereZoomedIn():
 
     # Mars Atmosphere
     h_mars = np.linspace(0, 100, 100)
-    p_mars = .699*np.exp(-.00009*h_mars*1000)
+    p_mars = 0.699 * np.exp(-0.00009 * h_mars * 1000)
 
     fig, ax1 = plt.subplots(figsize=(4.5, 8))
     # Earth
-    color = 'royalblue'
-    ax1.set_xlabel('Pressure (kPa)')
-    ax1.set_ylabel('Earth Altitude (km)', color=color)
-    ax1.plot(atmo.P/1000, h, color=color)
-    ax1.tick_params(axis='y', labelcolor=color)
+    color = "royalblue"
+    ax1.set_xlabel("Pressure (kPa)")
+    ax1.set_ylabel("Earth Altitude (km)", color=color)
+    ax1.plot(atmo.P / 1000, h, color=color)
+    ax1.tick_params(axis="y", labelcolor=color)
     ax1.set_ylim([34, 100])
 
     ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
     # Mars
-    color = 'coral'
-    ax2.set_ylabel('Mars Altitude (km)', color=color)
+    color = "coral"
+    ax2.set_ylabel("Mars Altitude (km)", color=color)
     ax2.plot(p_mars, h_mars, color=color)
-    ax2.tick_params(axis='y', labelcolor=color)
+    ax2.tick_params(axis="y", labelcolor=color)
 
-    plt.xlim([0, .7])
+    plt.xlim([0, 0.7])
     plt.ylim([0, 100])
     fig.tight_layout()
-    plt.savefig("./small_atmo.svg", transparent=True, bbox_inches='tight')
+    plt.savefig("./small_atmo.svg", transparent=True, bbox_inches="tight")
 
 
 # DesignVehicle()
-# DrawSupersonicWing(.1, 2.5, 4, gamma)
-Atmosphere()
-AtmosphereZoomedIn()
+# DrawSupersonicWing(0.06, 1.2, 1.6, gamma)
+# Atmosphere()
+# AtmosphereZoomedIn()
